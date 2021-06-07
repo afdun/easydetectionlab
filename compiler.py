9b8cf465b109eced34be494d73c40c3af7945ea8
Amaury Favier du Noyer <amaury.favier.du.noyer@eisti.fr>
2021-06-07 10:00:38 +0200

    Fix bug winrm and add pdoc comments

--- compiler.py
+++ compiler.py
@@ -1,195 +1,103 @@
+
+
+# IMPORT
 import json
 
-dic = {
-  "'logger'": {
-    "cfg": {
-        "vm":{
-          "box": "'bento/ubuntu-18.04'",
-          "hostname": "'logger'",
-          "provision": [
-            ":shell, path: 'logger_bootstrap.sh'",
-          ],
-          "network": [
-            ":private_network, ip: '192.168.38.105', gateway: '192.168.38.1', dns: '8.8.8.8'"
-          ],
-          "provider": "'virtualbox'",
-          "vb": {
-            "gui": "true",
-            "name": "'logger'",
-            "customize": [
-              "['modifyvm',:id,'--memory',1024]",
-              "['modifyvm', :id, '--cpus', 1]",
-              "['modifyvm', :id, '--vram', '32']",
-              "['modifyvm', :id, '--nicpromisc2', 'allow-all']",
-              "['modifyvm', :id, '--clipboard', 'bidirectional']",
-              "['modifyvm', :id, '--natdnshostresolver1', 'on']",
-              "['setextradata', 'global', 'GUI/SuppressMessages', 'all' ]",
-            ]
-          }
-        }
-      }
-    },
-    "'wef'": {
-        "cfg": {
-            "vm": {
-                "box": "'detectionlab/win2016'",
-                "hostname": "'wef'",
-                "boot_timeout": "1000",
-                "communicator": "'winrm'",
-                "network": [
-                    ":private_network, ip: '192.168.38.103', gateway: '192.168.38.1', dns: '192.168.38.102'"
-                ],
-                "provision": [
-                    "'shell', path: 'scripts/fix-second-network.ps1', privileged: true, args: '-ip 192.168.38.103 -dns 8.8.8.8 -gateway 192.168.38.1'",
-                    "'shell', path: 'scripts/provision.ps1', privileged: false",
-                    "'reload'",
-                    "'shell', path: 'scripts/provision.ps1', privileged: false",
-                    "'shell', path: 'scripts/download_palantir_wef.ps1', privileged: false",
-                    "'shell', inline: 'wevtutil el | Select-String -notmatch 'Microsoft-Windows-LiveId' | Foreach-Object {wevtutil cl '$_'}', privileged: false",
-                    "'shell', path: 'scripts/install-wefsubscriptions.ps1', privileged: false",
-                    "'shell', path: 'scripts/install-splunkuf.ps1', privileged: false",
-                    "'shell', path: 'scripts/install-windows_ta.ps1', privileged: false",
-                    "'shell', path: 'scripts/install-utilities.ps1', privileged: false",
-                    "'shell', path: 'scripts/install-redteam.ps1', privileged: false",
-                    "'shell', path: 'scripts/install-evtx-attack-samples.ps1', privileged: false",
-                    "'shell', path: 'scripts/install-choco-extras.ps1', privileged: false",
-                    "'shell', path: 'scripts/install-osquery.ps1', privileged: false",
-                    "'shell', path: 'scripts/install-sysinternals.ps1', privileged: false",
-                    "'shell', path: 'scripts/install-velociraptor.ps1', privileged: false",
-                    "'shell', path: 'scripts/configure-pslogstranscriptsshare.ps1', privileged: false",
-                    "'shell', path: 'scripts/install-autorunstowineventlog.ps1', privileged: false",
-                    "'shell', inline: 'Set-SmbServerConfiguration -AuditSmb1Access $true -Force', privileged: false",
-                    "'shell', path: 'scripts/install-microsoft-ata.ps1', privileged: false",
-                    "'shell', inline: 'cscript c:\\windows\\system32\\slmgr.vbs /dlv', privileged: false"
-                ],
-                "provider": "'virtualbox'",
-                "vb": {
-                    "gui": "true",
-                    "name": "'wef.windomain.local'",
-                    "default_nic_type": "'82545EM'",
-                    "customize": [
-                        "['modifyvm', :id, '--memory', 2048]",
-                        "['modifyvm', :id, '--cpus', 1]",
-                        "['modifyvm', :id, '--vram', '32']",
-                        "['modifyvm', :id, '--clipboard', 'bidirectional']",
-                        "['modifyvm', :id, '--natdnshostresolver1', 'on']",
-                        "['setextradata', 'global', 'GUI/SuppressMessages', 'all' ]"
-                    ]
-                }
-            },
-            "winrm": {
-                "basic_auth_only": "true",
-                "timeout": "300",
-                "retry_limit": "20"
-            }
-        }
-    }
-}
 
-"""
-config.vm.define "wef" do |cfg|
-cfg.vm.box = "detectionlab/win2016"
-cfg.vm.hostname = "wef"
-cfg.vm.boot_timeout = 1000
-cfg.vm.communicator = "winrm"
-cfg.winrm.basic_auth_only = true
-cfg.winrm.timeout = 300
-cfg.winrm.retry_limit = 20
-cfg.vm.network :private_network, ip: "192.168.38.103", gateway: "192.168.38.1", dns: "192.168.38.102"
+# FUNCTIONS
+def getJSON(jsonFileName):
+    """
+    Function getting the content of a json file in a python json
 
-cfg.vm.provision "shell", path: "scripts/fix-second-network.ps1", privileged: true, args: "-ip 192.168.38.103 -dns 8.8.8.8 -gateway 192.168.38.1"
-cfg.vm.provision "shell", path: "scripts/provision.ps1", privileged: false
-cfg.vm.provision "reload"
-cfg.vm.provision "shell", path: "scripts/provision.ps1", privileged: false
-cfg.vm.provision "shell", path: "scripts/download_palantir_wef.ps1", privileged: false
-cfg.vm.provision "shell", inline: 'wevtutil el | Select-String -notmatch "Microsoft-Windows-LiveId" | Foreach-Object {wevtutil cl "$_"}', privileged: false
-cfg.vm.provision "shell", path: "scripts/install-wefsubscriptions.ps1", privileged: false
-cfg.vm.provision "shell", path: "scripts/install-splunkuf.ps1", privileged: false
-cfg.vm.provision "shell", path: "scripts/install-windows_ta.ps1", privileged: false
-cfg.vm.provision "shell", path: "scripts/install-utilities.ps1", privileged: false
-cfg.vm.provision "shell", path: "scripts/install-redteam.ps1", privileged: false
-cfg.vm.provision "shell", path: "scripts/install-evtx-attack-samples.ps1", privileged: false
-cfg.vm.provision "shell", path: "scripts/install-choco-extras.ps1", privileged: false
-cfg.vm.provision "shell", path: "scripts/install-osquery.ps1", privileged: false
-cfg.vm.provision "shell", path: "scripts/install-sysinternals.ps1", privileged: false
-cfg.vm.provision "shell", path: "scripts/install-velociraptor.ps1", privileged: false
-cfg.vm.provision "shell", path: "scripts/configure-pslogstranscriptsshare.ps1", privileged: false
-cfg.vm.provision "shell", path: "scripts/install-autorunstowineventlog.ps1", privileged: false
-cfg.vm.provision "shell", inline: "Set-SmbServerConfiguration -AuditSmb1Access $true -Force", privileged: false
-cfg.vm.provision "shell", path: "scripts/install-microsoft-ata.ps1", privileged: false
-cfg.vm.provision "shell", inline: 'cscript c:\windows\system32\slmgr.vbs /dlv', privileged: false
+    @param `jsonFileName`: name of the json file to get the content
 
+    @return `jsonText`: json with the json content
+    """
+    with open(jsonFileName) as jsonFile:
+        jsonText = json.load(jsonFile)
+    return jsonText
 
-
-cfg.vm.provider "virtualbox" do |vb, override|
-vb.gui = true
-vb.name = "wef.windomain.local"
-vb.default_nic_type = "82545EM"
-vb.customize ["modifyvm", :id, "--memory", 2048]
-vb.customize ["modifyvm", :id, "--cpus", 1]
-vb.customize ["modifyvm", :id, "--vram", "32"]
-vb.customize ["modifyvm", :id, "--clipboard", "bidirectional"]
-vb.customize ["modifyvm", :id, "--natdnshostresolver1", "on"]
-vb.customize ["setextradata", "global", "GUI/SuppressMessages", "all" ]
-end
-"""
-
-
-
-def getJSON(fileName):
-    with open(fileName) as jsonFile:
-        infraJSON = json.load(jsonFile)
-    return infraJSON
 
 def setVAGRANTFILE(vagrantFileName, vagrantText):
+    """
+    Function writing a vagrant text in a vagrant file
+
+    @param `vagrantFileName`: name of the vagrant file to write to
+    @param `vagrantText`: vagrant text to write in the vagrant file
+
+    @return `vagrantFile.closed`: boolean (True if the vagrant file successfully closed else False)
+    """
     with open(vagrantFileName,'w') as vagrantFile:
         vagrantFile.write(vagrantText)
     return vagrantFile.closed
 
-def printJSON(textJSON):
-    print(json.dumps(textJSON,indent=2))
 
-def configVM(name,config,vagrantFile):
-    vagrantFile += "  config.vm.define {} do |cfg|\n".format(name)
+def printJSON(jsonText):
+    """
+    Function printing a python json in the console (useful for debug, not production)
+
+    @param `jsonText`: json text to print
+
+    """
+    print(json.dumps(jsonText,indent=2))
+
+
+def configVM(name,config,vagrantText):
+    """
+    Function converting one item of th
+
+    @param `jsonFileName`: name of the json file to write to
+    @param `jsonText`: json text to write in the json file
+
+    @return `jsonFile.closed`: boolean (True if the json file successfully closed else False)
+    """
+    vagrantText += "  config.vm.define {} do |cfg|\n".format(name)
     for cfg,value in config.items():
         for vm,value2 in value.items():
             for setting,value3 in value2.items():
-                if setting == "provision" or setting == "network":
+                if setting == "provision"::
                     for i in value3:
-                        vagrantFile += 4*' '+'.'.join((cfg,vm,setting))+' '+i+'\n'
+                        vagrantText += 4*' '+'.'.join((cfg,vm,setting))+' '+i+'\n'
                 elif setting == "vb":
                     for settingVB,value4 in value3.items():
                         if settingVB != "customize":
-                            vagrantFile += 6*' '+'.'.join((setting,settingVB))+' = '+value4+'\n'
+                            vagrantText += 6*' '+'.'.join((setting,settingVB))+' = '+value4+'\n'
                         else:
                             for i in value4:
-                                vagrantFile += 6*' '+'.'.join((setting,settingVB))+' '+i+'\n'
-                    vagrantFile += 4*' '+"end\n"
+                                vagrantText += 6*' '+'.'.join((setting,settingVB))+' '+i+'\n'
+                    vagrantText += 4*' '+"end\n"
                 elif setting == "provider":
-                    vagrantFile += 4*' '+'.'.join((cfg,vm,setting))+' '+value3+' do |vb, override|\n'
+                    vagrantText += 4*' '+'.'.join((cfg,vm,setting))+' '+value3+' do |vb, override|\n'
                 else:
-                    vagrantFile += 4*' '+'.'.join((cfg,vm,setting))+' = '+value3+'\n'
-    vagrantFile += "  end\n"
-    return vagrantFile
+                    vagrantText += 4*' '+'.'.join((cfg,vm,setting))+' = '+value3+'\n'
+    vagrantText += "  end\n"
+    return vagrantText
 
+def convertJSONtoVAGRANTFILE(jsonFileName):
+    """ Function converting a json file in vagrant text
 
-"""
-    Function that converts the JSON infrastructure in a VagrantFile
-    @return result : boolean (True if successfully created VagrantFile, False else)
-"""
-def convertJSONtoVAGRANTFILE(jsonFile):
-    vagrantFile = "Vagrant.configure('2') do |config|\n"
-    infraJSON = getJSON('infra.json')
-    for nameVM, config in infraJSON.items():
-        vagrantFile = configVM(nameVM,config,vagrantFile)
-    vagrantFile += "end\n"
-    return vagrantFile
+    @param `jsonFileName`: name of the json file to convert to vagrant text
 
+    @return `vagrantText`: string with the converted vagrant text
+    """
+    jsonText = getJSON('infra.json')
+    vagrantText = ""
+    if "intro" in jsonText:
+        for line in jsonText["intro"]:
+            vagrantText += line + "\n"
+    vagrantText += "Vagrant.configure('2') do |config|\n"
+    for nameVM, config in jsonText.items():
+        if nameVM == "intro":
+            continue
+        vagrantText = configVM(nameVM,config,vagrantText)
+    vagrantText += "end\n"
+    return vagrantText
+
+# MAIN
 if __name__ == "__main__":
-    # printJSON(dic)
-    jsonFile = "infra.json"
+    jsonFileName = "infra.json"
     vagrantFileName = "VagrantFile"
-    vagrantText = convertJSONtoVAGRANTFILE(jsonFile)
-    print(vagrantText)
+    vagrantText = convertJSONtoVAGRANTFILE(jsonFileName)
+    # print(vagrantText)
     result = setVAGRANTFILE(vagrantFileName, vagrantText)
     print("Ecriture du fichier ", vagrantFileName, " : ", result)
--- decompiler.py
+++ decompiler.py
@@ -1,27 +1,72 @@
+"""
+This decompiler converts a VagrantFile to a jsonFile.
+"""
+
+# IMPORT
 import re
 import json
 
+# FUNCTIONS
 def setJSON(jsonFileName, jsonText):
+    """
+    Function writing a python json in a json file with json format
+
+    @param `jsonFileName`: name of the json file to write to
+    @param `jsonText`: json text to write in the json file
+
+    @return `jsonFile.closed`: boolean (True if the json file successfully closed else False)
+    """
     with open(jsonFileName,'w') as jsonFile:
         jsonFile.write(json.dumps(jsonText, indent=2))
     return jsonFile.closed
 
-def getVAGRANTFILE(vagrantFileName):
-    with open(vagrantFileName) as VagrantFile:
-        VagrantFile.read()
-    return VagrantFile.closed
 
-def printJSON(jsonFile):
-    print(json.dumps(jsonFile, indent=2))
+def getVAGRANTFILE(vagrantFileName):
+    """
+    Function getting the content of a VagrantFile (not used but for potential future use)
+
+    @param `vagrantFileName`: name of the vagrant file to get the content
+
+    @return `vagrantText`: string with the vagrant content
+    """
+    with open(vagrantFileName) as vagrantFile:
+        vagrantText = vagrantFile.read()
+    return vagrantText
+
+
+def printJSON(jsonText):
+    """
+    Function printing a python json in the console (useful for debug, not production)
+
+    @param `jsonText`: json text to print
+
+    """
+    print(json.dumps(jsonTextndent=2))
+
 
 def calcNumberIndent(line):
-    for i in range(len(line)):
-        if line[i] != " ":
-            break
-    return i
+    """
+    Function calculating the number of indents at the beginning of a given string
 
-def verifIndent(file):
-    with open(file,"r") as vagrantfile:
+    @param `line`: string
+
+    @return `numberIndent`: integer number of indents at the beginning of the given string
+    """
+    for numberIndent in range(len(line)):
+        if line[numberIndent] != " ":
+            break
+    return numberIndent
+
+def verifIndent(vagrantFileName):
+    """
+    Function verifying the correct indent format of a vagrant file
+
+    @param `vagrantFileName`: name of the vagrant file to verify
+
+    @return `True`: boolean if the vagrant file has correct indentation
+    @return `indentErrorMessage` : string with the number of the line badly indented else
+    """
+    with open(vagrantFileName,"r") as vagrantfile:
         previousIndent = 0
         numberLine = 0
         for line in vagrantfile:
@@ -30,55 +75,82 @@ def verifIndent(file):
                 continue
             newIndent = calcNumberIndent(line)
             diff = abs(newIndent - previousIndent)
-            # print("Previous : ", previousIndent, "New : ", newIndent, "diff : ", diff)
             if diff != 2 and diff != 0:
-                # return "Vérification de l'indentation: échec. Issue line {}.".format(numberLine)
-                return numberLine
+                indentErrorMessage = "error line {}".format(numberLine)
+                return indentErrorMessage
             previousIndent = newIndent
-    # return "Vérification de l'indentation: succès."
     return True
 
 
-def convertVAGRANTFILEtoJSON(file):
-    with open(file,"r") as vagrantfile:
-        jsonFile = {}
+def convertVAGRANTFILEtoJSON(vagrantFileName):
+    """
+    Function converting a vagrant file in a python json
+
+    @param `vagrantFileName`: name of the vagrant file to convert to json
+
+    @return `jsonText: json
+    """
+    with open(vagrantFileName,"r") as vagrantfile:
+        jsonText = {}
         indent = 0
+        intro = True # position of reading first lines before configuration
         for line in vagrantfile:
             if not line.strip():
                 continue
 
             indent = calcNumberIndent(line)
 
+            if line.startswith("Vagrant.configure(",indent):
+                intro = False
+
+            if intro:
+                if "intro" not in jsonText:
+                    jsonText["intro"] = [line]
+                else:
+                    jsonText["intro"].append(line)
+
             if line.startswith("config.vm.define",indent):
                 regex = re.compile("\s\".+\"\s")
                 matchObject = regex.search(line)
                 if matchObject:
                     nameVM = matchObject.group(0).strip()
-                    jsonFile[nameVM] = { "cfg" : {}}
-                    newVM = jsonFile[nameVM]["cfg"]
+                    jsonText[nameVM] = { "cfg" : {}}
+                    newVM = jsonText[nameVM]["cfg"]
 
-            if line.startswith("cfg.vm.",indent):
-                regex = re.compile("cfg\.vm\.(\w+)[\s\=]+(.+)")
+            # if line.startswith("cfg.winrm",indent):
+            #     regex = re.compile("cfg.")
+            #     matchObject = regex.search(line)
+            #     if matchObject:
+            #         nameVM = matchObject.group(0).strip()
+            #         jsonText[nameVM] = { "cfg" : {}}
+            #         newVM = jsonText[nameVM]["cfg"]
+
+            if line.startswith("cfg.",indent):
+                regex = re.compile("cfg\.((?:vm)?(?:winrm)?)\.(\w+)[\s\=]+(.+)")
+                # regex = re.compile("cfg\.vm\.(\w+)[\s\=]+(.+)")
                 matchObject = regex.search(line)
 
                 if matchObject:
-                    nameParamVM = matchObject.group(1).strip()
-                    paramVM = matchObject.group(2).strip()
+                    vmOrWinrm = matchObject.group(1).strip()
+                    nameParamVM = matchObject.group(2).strip()
+                    paramVM = matchObject.group(3).strip()
+                    if vmOrWinrm not in newVM:
+                        newVM[vmOrWinrm] = {}
 
                     if nameParamVM == "provider":
                         if paramVM[1:11] == "virtualbox":
                             paramVM = "\"virtualbox\""
-                            newVM[nameParamVM] = paramVM
-                            newVM["vb"] = {}
-                            newVB = newVM["vb"]
+                            newVM[vmOrWinrm][nameParamVM] = paramVM
+                            newVM[vmOrWinrm]["vb"] = {}
+                            newVB = newVM[vmOrWinrm]["vb"]
                     else:
                         if nameParamVM == "provision":
                             if nameParamVM not in newVM:
-                                newVM[nameParamVM] = [paramVM]
+                                newVM[vmOrWinrm][nameParamVM] = [paramVM]
                             else:
-                                newVM[nameParamVM].append(paramVM)
+                                newVM[vmOrWinrm][nameParamVM].append(paramVM)
                         else:
-                            newVM[nameParamVM] = paramVM
+                            newVM[vmOrWinrm][nameParamVM] = paramVM
 
             if line.startswith("vb.",indent):
                 regex = re.compile("vb\.(\w+)[\s\=]+(.+)")
@@ -95,7 +167,7 @@ def convertVAGRANTFILEtoJSON(file):
                     else:
                         newVB[nameParamVB] = paramVB
 
-    return jsonFile
+    return jsonText
 
 
 if __name__ == "__main__":
@@ -103,7 +175,8 @@ if __name__ == "__main__":
     jsonFileName = "TEMP-jsonfile.json"
     result1 = verifIndent(vagrantFileName)
     print("Vérification de l'indentation : ", result1)
-    jsonText = convertVAGRANTFILEtoJSON(vagrantFileName)
-    printJSON(jsonText)
-    result2 = setJSON(jsonFileName, jsonText)
-    print("Ecriture du fichier ",jsonFileName," : ", result2)
+    if result1 == True:
+        jsonText = convertVAGRANTFILEtoJSON(vagrantFileName)
+        # printJSON(jsonText)
+        result2 = setJSON(jsonFileName, jsonText)
+        print("Ecriture du fichier ",jsonFileName," : ", result2)
